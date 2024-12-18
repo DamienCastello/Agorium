597ab20b4669a6a1cebc3f569982b3be1a3c82c8
Gamma <d.castello.13200@gmail.com>
2024-12-18 18:16:07 +0100

    feat(init-front-vue): implement Articles and Player without router

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <ul>
+            <li><strong>Wake up normies</strong></li>
+        </ul>
+        <ul>
+            <li><RouterLink to="/articles">Go to Articles</RouterLink></li>
+            <li><RouterLink to="/new-article">Go to post</RouterLink></li>
+            <li>
+            <details class="dropdown">
+                <summary>
+                Account
+                </summary>
+                <ul dir="rtl">
+                    <li><RouterLink to="/">Go to my profil</RouterLink></li>
+                    <li><RouterLink to="/logout">Logout</RouterLink></li>
+                </ul>
+            </details>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ frontend/src/utils/url.js
@@ -0,0 +1,5 @@
+export default {
+    baseUrl : 'http://localhost',
+    portBack: '3000',
+    portFront: '5173'
+};
