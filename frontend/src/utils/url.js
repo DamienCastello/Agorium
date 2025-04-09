238a5265b2730723bf7abba8c6d0ca8529d5d4ef
Gamma <d.castello.13200@gmail.com>
2025-04-09 14:36:38 +0200

    feat(dockerize-project): configure pre-prod to deploy

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,34 +1,17 @@
 const getBaseUrl = () => {
-    // Vérifie l'environnement et renvoie la base URL correspondante
-    if (process.env.NODE_ENV === 'production') {
-        return process.env.BASE_URL;
-    } else if (process.env.NODE_ENV === 'pre-prod') {
-        return process.env.BASE_URL;
-    }
-    // Par défaut, pour dev
-    return process.env.BASE_URL;
+  return window.environment.VITE_BASE_URL;
 };
 
 const getBackPort = () => {
-    if (process.env.NODE_ENV === 'production') {
-        return process.env.PORT_BACK;
-    } else if (process.env.NODE_ENV === 'pre-prod') {
-        return process.env.PORT_BACK;
-    }
-    return process.env.PORT_BACK;
+  return window.environment.VITE_PORT_BACK;
 };
 
 const getFrontPort = () => {
-    if (process.env.NODE_ENV === 'production') {
-        return process.env.PORT_FRONT;
-    } else if (process.env.NODE_ENV === 'pre-prod') {
-        return process.env.PORT_FRONT;
-    }
-    return process.env.PORT_FRONT;
+  return window.environment.VITE_PORT_FRONT;
 };
 
 export default {
-    baseUrl: getBaseUrl(),
-    portBack: getBackPort(),
-    portFront: getFrontPort()
+  baseUrl: getBaseUrl(),
+  portBack: getBackPort(),
+  portFront: getFrontPort()
 };
