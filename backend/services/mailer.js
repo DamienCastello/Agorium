ca3e68092dd96b3b59b6f899d06959e2d00e6613
Gamma <d.castello.13200@gmail.com>
2025-10-02 14:33:48 +0200

    fix(smtp): fix transporter

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -4,18 +4,25 @@ const i18n = require('../config/i18n-config');
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
+    port: Number(process.env.SMTP_PORT),  // 587
+    secure: false,                        // STARTTLS
+    requireTLS: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
-    }
+    },
   });
+
+  transporter.verify()
+  .then(() => console.log('[mailer] SMTP ok'))
+  .catch(err => console.error('[mailer] SMTP error', err));
   
 exports.sendVerificationEmail = async (to, link, lang) => {
-  const t = (...args) => i18n.__({ phrase: args[0], locale: lang });
+  const t = (...args) => i18n.__({ phrase: args[0], locale: lang || 'en' });
   const subject = t('auth.mailer_verification.subject');
   const html = t('auth.mailer_verification.html');
-  return transporter.sendMail({
+
+  const info = await transporter.sendMail({
     from: `"Agorium" <${process.env.EMAIL_FROM}>`,
     to,
     subject: subject,
@@ -24,16 +31,21 @@ exports.sendVerificationEmail = async (to, link, lang) => {
       <a href="${link}">${link}</a>
     `
   });
+  console.log('[mailer] Verification sent:', info.messageId, info.response);
+  return info;
 };
 
  exports.sendPasswordResetEmail = async (to, link, lang) => {
-  const t = (...args) => i18n.__({ phrase: args[0], locale: lang });
+  const t = (...args) => i18n.__({ phrase: args[0], locale: lang || 'en' });
   const subject = t('auth.mailer_reset.subject');
   const html = t('auth.mailer_reset.html');
-    await transporter.sendMail({
-      from: `"Agorium" <${process.env.MAIL_USER}>`,
-      to,
-      subject: subject,
-      html: `${html} <a href="${link}">${link}</a></p>`,
-    });
+
+  const info = await transporter.sendMail({
+    from: `"Agorium" <${process.env.EMAIL_FROM}>`,
+    to,
+    subject: subject,
+    html: `${html} <a href="${link}">${link}</a></p>`,
+  });
+  console.log('[mailer] Reset sent:', info.messageId, info.response);
+  return info;
   }
